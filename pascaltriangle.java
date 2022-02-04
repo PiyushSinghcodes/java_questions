8e413a565ab05395024b15d553d994401a399da1
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2022-02-05 04:25:23 +0530

    ok

---
+++ pascaltriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pascaltriangle {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter number of lines");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+          for(int j=1;j<=n-i;j++)
+          {
+              System.out.print(" ");
+          }
+          String s=(int)Math.pow(11,i-1)+"";
+          for(int j=0;j<s.length();j++)
+          { 
+              s=s.substring(j);
+            System.out.print((Character.getNumericValue(s.charAt(i)))+" ");
+          }
+          System.out.println("");
+        }
+    }
+}
