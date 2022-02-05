faecf6eea86bd58f4652d2bf3ee79653a53f5783
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2022-02-05 06:21:08 +0530

    cha

---
+++ tableof11.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class tableof11 {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter number of lines");
+        int n=sc.nextInt();
+        BigInteger a=new BigInteger("1");
+        BigInteger b=new BigInteger("11");
+        for(int i=1;i<=n;i++)
+        {
+          for(int j=1;j<=n-i;j++)
+          {
+              System.out.print(" ");
+          }
+          String s=a+"";
+          a=a.multiply(b);
+          for(int j=0;j<s.length();j++)
+          { 
+            System.out.print((Character.getNumericValue(s.charAt(j)))+" ");
+          }
+          System.out.println("");
+        }
+    }
+}
