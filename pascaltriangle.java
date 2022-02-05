094eb0ce076ceae6b920c22758f85624b6a40264
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2022-02-05 06:13:51 +0530

    pascalerror

--- pascaltriangle.java
+++ pascaltriangle.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 public class pascaltriangle {
     public static void main(String[] args)
@@ -5,16 +6,19 @@ public class pascaltriangle {
         Scanner sc=new Scanner(System.in); 
         System.out.println("Enter number of lines");
         int n=sc.nextInt();
+        BigInteger a=new BigInteger("1");
+        BigInteger b=new BigInteger("11");
         for(int i=1;i<=n;i++)
         {
           for(int j=1;j<=n-i;j++)
           {
               System.out.print(" ");
           }
-          String s=(int)Math.pow(11,i-1)+"";
+          String s=a+"";
+          a=a.multiply(b);
           for(int j=0;j<s.length();j++)
           { 
-            System.out.print((Character.getNumericValue(s.charAt(i)))+" ");
+            System.out.print((Character.getNumericValue(s.charAt(j)))+" ");
           }
           System.out.println("");
         }
