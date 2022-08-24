e8212e624d59f3def06a2b72f69ddb199547af37
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2022-08-24 20:39:08 +0530

    c

---
+++ stocksellandbuy.java
@@ -0,0 +1,12 @@
+public class class stocksellandbuy {
+    
+    public static void main(String[] args) {
+            System.out.println("Hello, World!");
+            int[] a={1,5,0,8,12};
+            sell(a,5);
+        
+    }
+   public static void  {
+       
+   } 
+}
